83f233f769442153bd95d49cbb5ad50fa2af4818
Denys Okhremenko <denys.okhremenko@makeheadway.com>
2024-05-05 15:36:45 +0300

    Initial Commit

---
+++ NextButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextButton
+//
+//  Created by Denys Okhremenko on 05.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextButton/NextButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextButtonApp.swift
+//  NextButton
+//
+//  Created by Denys Okhremenko on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NextButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
