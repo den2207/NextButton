ad97847f7ba25996b3dc572e3883912ec00db15f
Denys Okhremenko <denys.okhremenko@makeheadway.com>
2024-05-07 14:44:29 +0300

    initial commit

--- NextButton/ContentView.swift
+++ NextButton/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTapped = false
+    @State var animationCount = 3
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                showAnimate()
+            }, label: {
+                HStack(spacing: .zero) {
+                    Image(systemName: "arrowtriangle.forward.fill")
+                        .resizable()
+                        .opacity(isTapped ? 1 : 0)
+                        .frame(width: isTapped ? 20 : 0, height: isTapped ? 20 : 0)
+                    Image(systemName: "arrowtriangle.forward.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Image(systemName: "arrowtriangle.forward.fill")
+                        .resizable()
+                        .opacity(isTapped ? 0 : 1)
+                        .frame(width: isTapped ? 1 : 20, height: isTapped ? 1 : 20)
+                }
+            })
         }
         .padding()
+    }
+    
+    func showAnimate() {
+        animationCount -= 1
+        withAnimation(.smooth(extraBounce: 0.4)) {
+            isTapped = true
+        } completion: {
+            isTapped = false
+            if animationCount > 0 {
+                showAnimate()
+            } else {
+                animationCount = 3
+            }
+        }
     }
 }
 
